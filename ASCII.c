e706b50c1e8ad2981b11d3811ba9a0fede9e7fea
Aditya Chowdhury <143079032+Neuvillete@users.noreply.github.com>
2023-09-11 02:35:35 +0530

    Create ASCII.c
    This is how you check the ASCII value of things.

---
+++ ASCII.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    char a;
+
+    printf("Enter a word to see its ASCII value ");
+    scanf("%c", &a);
+
+    printf("The ASCII Value of %c is %d", a ,a);
+
+    return 0;
+}
